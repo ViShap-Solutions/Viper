89cb2f6ae43a17df73fe47e374458789734c91e2
0xVahe <vahe.babayan.dev@gmail.com>
2026-09-13 15:22:36 +0400

    Char low-surrogate data corruption fixed

--- src/ViShap.Viper.Serialization/Formatters/BuiltInFormatters.cs
+++ src/ViShap.Viper.Serialization/Formatters/BuiltInFormatters.cs
@@ -14,7 +14,7 @@ internal static class PrimitiveFormatters
     public static readonly ITypeFormatter Float = new PrimitiveFormatter<float>((w, v) => w.Write(v), r => r.ReadSingle());
     public static readonly ITypeFormatter Double = new PrimitiveFormatter<double>((w, v) => w.Write(v), r => r.ReadDouble());
     public static readonly ITypeFormatter Decimal = new PrimitiveFormatter<decimal>((w, v) => w.Write(v), r => r.ReadDecimal());
-    public static readonly ITypeFormatter Char = new PrimitiveFormatter<char>((w, v) => w.Write(v), r => r.ReadChar());
+    public static readonly ITypeFormatter Char = new PrimitiveFormatter<char>((w, v) => w.Write((ushort)v), r => (char)r.ReadUInt16());
     public static readonly ITypeFormatter String = new PrimitiveFormatter<string>((w, v) => w.Write(v), r => r.ReadString());
     public static readonly ITypeFormatter Enum = new EnumFormatter();
     public static readonly ITypeFormatter Half = new HalfFormatter();
