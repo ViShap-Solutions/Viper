8b767bbb10fc1946b135c5767a96c0ae5ffca2c9
0xVahe <vahe.babayan.dev@gmail.com>
2026-09-16 21:51:24 +0400

    FrozenFactoryCache fixed

--- src/ViShap.Viper.Serialization/Cache/FrozenFactoryCache.cs
+++ src/ViShap.Viper.Serialization/Cache/FrozenFactoryCache.cs
@@ -16,19 +16,47 @@ internal static class FrozenFactoryCache
             var sourceType = typeof(IEnumerable<>).MakeGenericType(
                 typeof(KeyValuePair<,>).MakeGenericType(key.Key, key.Value));
 
-            var method = typeof(FrozenDictionary).GetMethods(BindingFlags.Public | BindingFlags.Static)
-                .Where(static m => m.Name == nameof(FrozenDictionary.ToFrozenDictionary))
-                .Where(static m => m.IsGenericMethodDefinition)
-                .Where(m => m.GetGenericArguments().Length == 2)
-                .SingleOrDefault(m =>
-                    m.GetParameters().Length >= 1 &&
-                    m.GetParameters()[0].ParameterType == sourceType &&
-                    (m.GetParameters().Length == 1 ||
-                     (m.GetParameters().Length == 2 && m.GetParameters()[1].IsOptional)))
-                ?? throw new BinaryTypeException(
-                    $"Could not resolve FrozenDictionary.ToFrozenDictionary<{key.Key.Name}, {key.Value.Name}>().");
+            var methods = typeof(FrozenDictionary)
+                              .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                              .Where(static m =>
+                                  m.Name == nameof(FrozenDictionary.ToFrozenDictionary))
+                              .Where(static m =>
+                                  m.IsGenericMethodDefinition &&
+                                  m.GetGenericArguments().Length == 2)
+                              .Where(static m =>
+                              {
+                                  var parameters = m.GetParameters();
 
-            var closedMethod = method.MakeGenericMethod(key.Key, key.Value);
+                                  if (parameters.Length != 2)
+                                      return false;
+
+                                  if (!parameters[1].IsOptional)
+                                      return false;
+
+                                  var sourceParameter = parameters[0].ParameterType;
+
+                                  if (!sourceParameter.IsGenericType ||
+                                      sourceParameter.GetGenericTypeDefinition() != typeof(IEnumerable<>))
+                                      return false;
+
+                                  var elementParameter =
+                                      sourceParameter.GetGenericArguments()[0];
+
+                                  if (!elementParameter.IsGenericType ||
+                                      elementParameter.GetGenericTypeDefinition() != typeof(KeyValuePair<,>))
+                                      return false;
+
+                                  var keyArguments = elementParameter.GetGenericArguments();
+                                  var methodArguments = m.GetGenericArguments();
+
+                                  return keyArguments[0] == methodArguments[0] &&
+                                         keyArguments[1] == methodArguments[1];
+                              })
+                              .SingleOrDefault()
+                          ?? throw new BinaryTypeException(
+                              $"Could not resolve FrozenDictionary.ToFrozenDictionary<{key.Key.Name}, {key.Value.Name}>().");
+
+            var closedMethod = methods.MakeGenericMethod(key.Key, key.Value);
             var param = Expression.Parameter(typeof(object), "source");
             var source = Expression.Convert(param, sourceType);
             Expression call = closedMethod.GetParameters().Length == 1
@@ -43,25 +71,49 @@ internal static class FrozenFactoryCache
         {
             var sourceType = typeof(IEnumerable<>).MakeGenericType(t);
 
-            var method = typeof(FrozenSet).GetMethods(BindingFlags.Public | BindingFlags.Static)
-                .Where(static m => m.Name == nameof(FrozenSet.ToFrozenSet))
-                .Where(static m => m.IsGenericMethodDefinition)
-                .Where(m => m.GetGenericArguments().Length == 1)
-                .SingleOrDefault(m =>
-                    m.GetParameters().Length >= 1 &&
-                    m.GetParameters()[0].ParameterType == sourceType &&
-                    (m.GetParameters().Length == 1 ||
-                     (m.GetParameters().Length == 2 && m.GetParameters()[1].IsOptional)))
-                ?? throw new BinaryTypeException(
-                    $"Could not resolve FrozenSet.ToFrozenSet<{t.Name}>().");
+            var method = typeof(FrozenSet)
+                             .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                             .Where(static m =>
+                                 m.Name == nameof(FrozenSet.ToFrozenSet))
+                             .Where(static m =>
+                                 m.IsGenericMethodDefinition &&
+                                 m.GetGenericArguments().Length == 1)
+                             .SingleOrDefault(m =>
+                             {
+                                 var parameters = m.GetParameters();
+
+                                 if (parameters.Length != 2)
+                                     return false;
+
+                                 if (!parameters[1].IsOptional)
+                                     return false;
+
+                                 var sourceParameter = parameters[0].ParameterType;
+
+                                 if (!sourceParameter.IsGenericType ||
+                                     sourceParameter.GetGenericTypeDefinition() != typeof(IEnumerable<>))
+                                     return false;
+
+                                 return sourceParameter.GetGenericArguments()[0] ==
+                                        m.GetGenericArguments()[0];
+                             })
+                         ?? throw new BinaryTypeException(
+                             $"Could not resolve FrozenSet.ToFrozenSet<{t.Name}>().");
 
             var closedMethod = method.MakeGenericMethod(t);
+
             var param = Expression.Parameter(typeof(object), "source");
             var source = Expression.Convert(param, sourceType);
-            Expression call = closedMethod.GetParameters().Length == 1
-                ? Expression.Call(closedMethod, source)
-                : Expression.Call(closedMethod, source, Expression.Default(closedMethod.GetParameters()[1].ParameterType));
 
-            return Expression.Lambda<Func<object, object>>(Expression.Convert(call, typeof(object)), param).Compile();
+            Expression call =
+                Expression.Call(
+                    closedMethod,
+                    source,
+                    Expression.Default(
+                        closedMethod.GetParameters()[1].ParameterType));
+
+            return Expression.Lambda<Func<object, object>>(
+                Expression.Convert(call, typeof(object)),
+                param).Compile();
         });
 }
